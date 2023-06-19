93d4aa85369019ee3cc8a145f6d4680e362906b4
khdfierror <108921408+khdfierror@users.noreply.github.com>
2023-06-19 23:00:35 +0800

    Update script.blade.php

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -80,7 +80,7 @@ $('body').on('click', '.tombol-edit', function(e) {
                 
                 $('#exampleModal').modal('show');
                 // $('#konten').text(response.result.konten);
-                $('#konten').text(doc);
+                $('#konten').innerHTML = doc;
                 $('#judul').text(response.result.judul);
             }
         });
